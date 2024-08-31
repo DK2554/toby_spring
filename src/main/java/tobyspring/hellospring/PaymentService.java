96957b84ade2f1ac0ffb8dfbb5e9da829df3d739
DK2554 <ldu0215@gmail.com>
2024-08-31 14:38:26 +0900

    2024_08_31
    토비 스프링

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,8 +1,17 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment perpaer(){
-        return new Payment();
+    public Payment perpaer(Long orderId, String currency, BigDecimal foreignCurrencyAmount){
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.perpaer(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
     }
 
 }
