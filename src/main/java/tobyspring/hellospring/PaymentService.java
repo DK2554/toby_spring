b0df2d516f933ca1b7679919b1271663ad96c5a0
DK2554 <ldu0215@gmail.com>
2024-09-04 20:45:49 +0900

    2024_09_04 토비 스프링
    관심사의 분리

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -14,7 +14,16 @@ import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment perpaer(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/USD"+currency);
+        BigDecimal exRate = getExRate(currency);
+
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    private BigDecimal getExRate(String currency) throws IOException {
+        URL url = new URL("https://open.er-api.com/v6/latest/USD"+ currency);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String collect = bufferedReader.lines().collect(Collectors.joining());
@@ -23,13 +32,9 @@ public class PaymentService {
         ObjectMapper mapper = new ObjectMapper();
         ExRateData data =mapper.readValue(collect, ExRateData.class);
         BigDecimal exRate = data.rates().get("KRW");
-        System.out.println(exRate);
-
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return exRate;
     }
+
     public static void main(String[] args) throws Exception{
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.perpaer(100L, "USD", BigDecimal.valueOf(50.7));
